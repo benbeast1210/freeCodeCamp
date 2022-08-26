007c028cd3649881f46abbb73739bfaace983dcd
Test User <testuser@Tests-MacBook-Pro.local>
2022-08-25 20:05:28 -0400

    added-react-lessons

---
+++ front_end_development_libraries/react/use_lifecycle_method_componentWillMount.js
@@ -0,0 +1,22 @@
+/* 
+React components have several special methods that provide opportunities to perform actions at specific points in the lifecycle of a component. These are called lifecycle methods, or lifecycle hooks, and allow you to catch components at certain points in time. This can be before they are rendered, before they update, before they receive props, before they unmount, and so on. Here is a list of some of the main lifecycle methods: componentWillMount() componentDidMount() shouldComponentUpdate() componentDidUpdate() componentWillUnmount() The next several lessons will cover some of the basic use cases for these lifecycle methods.
+
+Note: The componentWillMount Lifecycle method will be deprecated in a future version of 16.X and removed in version 17. Learn more in this article
+
+The componentWillMount() method is called before the render() method when a component is being mounted to the DOM. Log something to the console within componentWillMount() - you may want to have your browser console open to see the output.
+*/
+class MyComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  
+  componentWillMount() {
+    console.log("Hello world")
+  }
+
+  render() {
+    return <div />
+  }
+
+};
--- front_end_development_libraries/react/use_state_to_toggle_an_element.js
+++ front_end_development_libraries/react/use_state_to_toggle_an_element.js
@@ -32,27 +32,37 @@ class MyComponent extends React.Component {
     this.state = {
       visibility: false
     };
+
     this.toggleVisibility = this.toggleVisibility.bind(this);
+
    }
+
   toggleVisibility() {
     this.setState(state => ({
       visibility: !state.visibility
     }));
   }
+
   render() {
     if (this.state.visibility) {
+
       return (
         <div>
           <button onClick = {this.toggleVisibility}>Click Me</button>
           <h1>Now you see me!</h1>
         </div>
       );
+
     } else {
+
       return (
         <div>
           <button onClick = {this.toggleVisibility}>Click Me</button>
         </div>
       );
+
     }
+
   }
+
 };
